635dceac6c9242f1831dfac9e7f6703028912d44
Guy-Kaplan <39667435+Guy-Kaplan@users.noreply.github.com>
2018-06-02 13:04:13 +0300

    Update Logic.ts

--- shapesGame/Logic.ts
+++ shapesGame/Logic.ts
@@ -1,40 +1,52 @@
-﻿class Logic {
+class Logic {
 
-    public isSecondKeletTakin(num: number): boolean {
+    public isFirstKeletWrong(num: number): boolean {
+        if (num != 1 && num != 2 && num != 3 && num != 4 && num != 5 && num != 6 && num != 7 && num != 8 && num != 9 && num != 10 && num != 11) {
+            return true;
+        }
+        return false;
+    }
+
+    public isSecondKeletWrong(num: number): boolean {
         if (num < 1 || num != Math.floor(num)) {
-            return false;
+            return true;
         }
-        return true;
+        return false;
     }
 
-    public isThirdKeletTakin(num: number): boolean {
-        if (num != 1 && num != 2 && num != 3 && num != 4) {
-            return false;
+    public isThirdKeletWrong(num: number): boolean {
+        if (num != 1 && num != 2 && num != 3 && num != 4 && num != 5) {
+            return true;
         }
-        return true;
+        return false;
     }
 
-    public isIsoSecondKeletTakin(num: number): boolean {
-        //if (num < 1 || num != Math.floor(num)) {
+    public isFourthKeletWrong(shapeColor: string): boolean {
+        if (shapeColor === "") {
+            return true;
+        }
+        return false;
+    }
+
+    public isIsoSecondKeletWrong(num: number): boolean {
         if (num < 3 || num % 2 != 1 || num != Math.floor(num)) {
-            return false;
+            return true;
         }
-        return true;
+        return false;
     }
 
-    public isEllipseSecondKeletTakin(num: number): boolean {
-        //if (num < 1 || num != Math.floor(num)) {
-        if (num < 1 || num > 20) {
-            return false;
+    public isEllipseSecondKeletWrong(num: number): boolean {
+        if (num < 1 || num > 20 || isNaN(num) === true) {
+            return true;
         }
-        return true;
+        return false;
     }
 
-    public isCircleSecondKeletTakin(num: number): boolean {
-        if (num < 1 || num > 4) {
-            return false;
+    public isCircleSecondKeletWrong(num: number): boolean {
+        if (num < 1 || num > 4 || isNaN(num) === true) {
+            return true;
         }
-        return true;
+        return false;
     }
 
 
