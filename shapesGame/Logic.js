4df83ce89d5d11ec5fae07c5a5fa648575e4949e
Guy-Kaplan <39667435+Guy-Kaplan@users.noreply.github.com>
2018-06-02 13:04:54 +0300

    Update Logic.js

--- shapesGame/Logic.js
+++ shapesGame/Logic.js
@@ -1,37 +1,47 @@
 var Logic = /** @class */ (function () {
     function Logic() {
     }
-    Logic.prototype.isSecondKeletTakin = function (num) {
+    Logic.prototype.isFirstKeletWrong = function (num) {
+        if (num != 1 && num != 2 && num != 3 && num != 4 && num != 5 && num != 6 && num != 7 && num != 8 && num != 9 && num != 10 && num != 11) {
+            return true;
+        }
+        return false;
+    };
+    Logic.prototype.isSecondKeletWrong = function (num) {
         if (num < 1 || num != Math.floor(num)) {
-            return false;
+            return true;
         }
-        return true;
+        return false;
     };
-    Logic.prototype.isThirdKeletTakin = function (num) {
-        if (num != 1 && num != 2 && num != 3 && num != 4) {
-            return false;
+    Logic.prototype.isThirdKeletWrong = function (num) {
+        if (num != 1 && num != 2 && num != 3 && num != 4 && num != 5) {
+            return true;
         }
-        return true;
+        return false;
     };
-    Logic.prototype.isIsoSecondKeletTakin = function (num) {
-        //if (num < 1 || num != Math.floor(num)) {
+    Logic.prototype.isFourthKeletWrong = function (shapeColor) {
+        if (shapeColor === "") {
+            return true;
+        }
+        return false;
+    };
+    Logic.prototype.isIsoSecondKeletWrong = function (num) {
         if (num < 3 || num % 2 != 1 || num != Math.floor(num)) {
-            return false;
+            return true;
         }
-        return true;
+        return false;
     };
-    Logic.prototype.isEllipseSecondKeletTakin = function (num) {
-        //if (num < 1 || num != Math.floor(num)) {
-        if (num < 1 || num > 20) {
-            return false;
+    Logic.prototype.isEllipseSecondKeletWrong = function (num) {
+        if (num < 1 || num > 20 || isNaN(num) === true) {
+            return true;
         }
-        return true;
+        return false;
     };
-    Logic.prototype.isCircleSecondKeletTakin = function (num) {
-        if (num < 1 || num > 4) {
-            return false;
+    Logic.prototype.isCircleSecondKeletWrong = function (num) {
+        if (num < 1 || num > 4 || isNaN(num) === true) {
+            return true;
         }
-        return true;
+        return false;
     };
     return Logic;
 }());
